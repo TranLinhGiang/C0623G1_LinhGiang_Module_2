d128d4ec15c9b66a9c118b685079d285018bf8a6
Trần Linh Giang <gt07839@gmail.com>
2023-08-03 13:28:07 +0700

    Giang_nop_bai_tap_&_bai_thuc_hanh_bai_ss1

--- untitled/src/ss1_java_overview/bai_tap/numbersIntoWords.java
+++ untitled/src/ss1_java_overview/bai_tap/numbersIntoWords.java
@@ -5,40 +5,36 @@ import java.util.Scanner;
 public class numbersIntoWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] arr = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] arr2 = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        String[] arr3 = {"", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"};
+        String[] arr4 = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
         int number;
         System.out.println(" Enter number ");
         number = scanner.nextInt();
-        if (number < 10 && number >= 0) {
-           for(int i=0;i<arr.length;i++){
-               if(number==i){
-                   System.out.println(arr[i]);
-               }
-           }
+        int integerTens = number / 10; // số nguyên hàng chục
+        int balanceTens = number % 10;  // số dư hàng chục
+        int hundreds = number / 100; // số nguyên hàng trăm
+        int hundredTens = number % 100; // chia một trăm lấy dư
+
+        System.out.println(integerTens);
+        System.out.println(balanceTens);
+        if (number >= 0 && number < 10) {
+            System.out.println(arr[number]);
         }
-    if(number>13 && number<20){
-        for (int i=4;i<arr.length;i++){
-                System.out.println(arr[i]+"teen");
+        if (number >= 10 && number < 20) {
+            System.out.println(arr4[balanceTens]);
+        }
+        if (number > 20 && number < 100) {
+            System.out.println(arr2[integerTens] + " " + arr[balanceTens]);
+        }
+        if (number > 100 && number < 1000) {
+            if (hundredTens > 10 && hundredTens < 20) {
+                System.out.println(arr3[hundreds] +" "+ arr4[hundredTens % 10]);
+            } else {
+                System.out.println(arr3[hundreds] +" "+ arr2[hundredTens / 10] +" "+ arr[hundredTens % 10]);
             }
         }
-    if(number>=10 && number<=13){
-        switch (number){
-            case 10:
-                System.out.println("ten");
-                break;
-            case 11:
-                System.out.println("eleven");
-                break;
-            case 12:
-                System.out.println("twelve");
-                break;
-            case 13:
-                System.out.println("thirteen");
-        }
-    }
-        }
-
-
-
 
     }
+}
