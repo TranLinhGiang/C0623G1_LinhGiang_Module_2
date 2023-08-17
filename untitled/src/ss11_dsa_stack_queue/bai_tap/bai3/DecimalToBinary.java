c837ab9f877486455a550d750681c2ac2b95b6bc
Trần Linh Giang <gt07839@gmail.com>
2023-08-17 12:32:56 +0700

    Giang_submit_exercies_and_practice_ss11

--- untitled/src/ss11_dsa_stack_queue/bai_tap/bai3/DecimalToBinary.java
+++ untitled/src/ss11_dsa_stack_queue/bai_tap/bai3/DecimalToBinary.java
@@ -2,16 +2,16 @@ package ss11_dsa_stack_queue.bai_tap.bai3;
 
 public class DecimalToBinary {
     public String convertBinary(int num) {
-        int DeciMal[] = new int[num];
+        int deciMal[] = new int[num];
         int index = 0;
         while (num > 0) {
-            DeciMal[index] = num % 2;
+            deciMal[index] = num % 2;
             num = num / 2;
             index++;
         }
         String result = "";
         for (int i = index - 1; i >= 0; i--) {
-            result += DeciMal[i];
+            result += deciMal[i];
         }
         return result;
     }
