8cda27f3a44f6d67bde7474a1961ca447c5134df
Trần Linh Giang <gt07839@gmail.com>
2023-08-19 00:28:54 +0700

    Giang_submit_exercies_and_practice_ss13

--- untitled/src/ss13_search_algorithm/bai_tap/bai2/BinarySearch.java
+++ untitled/src/ss13_search_algorithm/bai_tap/bai2/BinarySearch.java
@@ -10,37 +10,41 @@ public class BinarySearch {
         int n = scanner.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            System.out.println("Nhập phần tử con trong mảng: ");
-            System.out.println(i);
+            System.out.println("Nhập phần tử con trong mảng: " + i);
             arr[i] = scanner.nextInt();
+
         }
-        System.out.println("Mảng vừa nhập là: ");
+        System.out.println(" Mảng của bạn vừa nhập và được sắp sếp là: ");
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
-        int value;
-        System.out.println("Nhập số bạn cần tìm: ");
-        value= scanner.nextInt();
+        BinarySearch main = new BinarySearch();
+        System.out.println("Nhập phần tử bạn cần tìm: ");
+        int value = scanner.nextInt();
 
-        BinarySearch binary = new BinarySearch();
-        int index = binary.binarySearch(arr, value, 0, arr.length - 1);
-        System.out.println(index);
+
+        int index = main.binarySearch(arr, 0, arr.length - 1, value);
+        System.out.println("Giá trị " + value + ": " + index);
     }
 
-    public int binarySearch(int[] array, int value, int left, int right) {
-        if (right >= left) {
-            int mid = (right + left) / 2;
-            if (array[mid] == value) {
-                return mid;
-            }
-            if (array[mid] > value) {
-                right = mid - 1;
-                return binarySearch(array, value, left, right);
-            } else {
-                left = mid + 1;
-                return binarySearch(array, value, left, right);
+    public int binarySearch(int[] arr, int left, int right, int value) {
+        int mid = left + (right - left) / 2;
+        if (value > 0) {
+            if (right >= left) {
+                if (arr[mid] == value) {
+                    return mid;
+                }
+                if (arr[mid] < value) {
+                    left = mid + 1;
+                    return binarySearch(arr, left, right, value);
+                } else {
+                    right = mid - 1;
+                    return binarySearch(arr, left, right, value);
+                }
             }
         }
-        return -1; // Không tìm thấy trả về -1.
+        return -1;
     }
 }
+
+
