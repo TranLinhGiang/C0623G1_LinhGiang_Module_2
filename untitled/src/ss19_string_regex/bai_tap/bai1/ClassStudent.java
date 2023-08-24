6c48c38a382eda203a4fa5062f42f9f37870988b
Trần Linh Giang <gt07839@gmail.com>
2023-08-24 14:25:40 +0700

    Giang_submit_exercies_and_practice_ss19

--- untitled/src/ss19_string_regex/bai_tap/bai1/ClassStudent.java
+++ untitled/src/ss19_string_regex/bai_tap/bai1/ClassStudent.java
@@ -1,2 +1,39 @@
-package ss19_string_regex.bai_tap.bai1;public class ClassStudent {
+package ss19_string_regex.bai_tap.bai1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassStudent {
+    private static Pattern pattern;
+
+    private static Matcher matcher;
+
+    private static final String CLASS_REGEX="^[C|A|P]+[0-9]{4}+[G|H|I|K]$";
+
+    public ClassStudent(){
+        pattern= Pattern.compile(CLASS_REGEX);
+    }
+    public boolean validate(String regex){
+        matcher= pattern.matcher(regex);
+        return matcher.matches();
+    }
+
+    public static class ClassStudentTest {
+        private static ClassStudent classStudent;
+        public static final  String[] validClass= new String[]{"C0223G","A0323K"};
+        public static final String[] invalidClass= new String[]{"M0318G","P0323A"};
+
+        public static void main(String[] args) {
+            classStudent= new ClassStudent();
+            for (String classes: validClass) {
+                boolean isValid= classStudent.validate(classes);
+                System.out.println("Classes is: "+classes+ "is valid: "+ isValid);
+            }
+
+            for (String classes: invalidClass) {
+                boolean isValid= classStudent.validate(classes);
+                System.out.println("Classes is: "+classes+ "is valid: "+ isValid);
+            }
+        }
+    }
 }
--- untitled/src/ss19_string_regex/bai_tap/bai2/NumberPhone.java
+++ untitled/src/ss19_string_regex/bai_tap/bai2/NumberPhone.java
@@ -1,2 +1,40 @@
-package ss19_string_regex.bai_tap.bai2;public class NumberPhone {
+package ss19_string_regex.bai_tap.bai2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumberPhone {
+    private static Pattern pattern;
+
+    private static Matcher matcher;
+
+    private static final String NUMBER_PHONE_REGEX="^[(]84[)]+[-(0]+[0-9]{9}+[)]$";
+
+    public NumberPhone(){
+        pattern= Pattern.compile(NUMBER_PHONE_REGEX);
+    }
+    public boolean validate(String regex){
+        matcher= pattern.matcher(regex);
+        return matcher.matches();
+    }
+
+    public static class TestNumberPhone {
+        private static NumberPhone numberPhone;
+        public static final String[] validPhoneNumber= new String[]{"(84)-(0978489648)"};
+        public static final String[] invalidPhoneNumber= new String[]{"(a8)-(22222222)"," (84)-(22b22222)"," (84)-(9978489648)"};
+
+        public static void main(String[] args) {
+            numberPhone= new NumberPhone();
+            for (String phone:
+                 validPhoneNumber) {
+                boolean isValid= numberPhone.validate(phone);
+                System.out.println("PhoneNumber: "+ phone + "is valid: "+ isValid);
+            }
+            for (String phone:
+                 invalidPhoneNumber) {
+                boolean isValid= numberPhone.validate(phone);
+                System.out.println("PhoneNumber: "+ phone + "is valid: "+ isValid);
+            }
+        }
+    }
 }
